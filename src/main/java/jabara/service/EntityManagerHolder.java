5da5ef5324600f05320e1ad0f4a202304bdb3721
Jabara <jabaraster@gmail.com>
2012-06-13 02:46:52 +0900

    mainte.

--- src/main/java/jabara/service/EntityManagerHolder.java
+++ src/main/java/jabara/service/EntityManagerHolder.java
@@ -29,15 +29,20 @@ public class EntityManagerHolder {
         return _emf.createEntityManager();
     }
 
+    public static void main(final String[] args) {
+        System.out.println(get());
+    }
+
     private static Map<String, String> createDbProperties() {
         try {
-            final URI dbUri = new URI(System.getenv("DATABASE_URL"));
+            final URI dbUri = new URI(getDbUrl());
 
             final String username = dbUri.getUserInfo().split(":")[0];
             final String password = dbUri.getUserInfo().split(":")[1];
             final String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath() + ":" + dbUri.getPort();
 
             final Map<String, String> ret = new HashMap<String, String>();
+            ret.put("javax.persistence.driver", "org.postgres.Driver");
             ret.put("javax.persistence.jdbc.url", dbUrl);
             ret.put("javax.persistence.jdbc.user", username);
             ret.put("javax.persistence.jdbc.password", password);
@@ -47,5 +52,20 @@ public class EntityManagerHolder {
         } catch (final URISyntaxException e) {
             throw new IllegalStateException(e);
         }
+    }
+
+    private static Map<String, String> createDbProperties2() {
+        final Map<String, String> ret = new HashMap<String, String>();
+
+        ret.put("javax.persistence.driver", "org.apache.derby.jdbc.ClientDriver");
+        ret.put("javax.persistence.jdbc.url", "jdbc:derby://localhost/app;create=true");
+        ret.put("javax.persistence.jdbc.user", "APP");
+        ret.put("javax.persistence.jdbc.password", "APP");
+
+        return ret;
+    }
+
+    private static String getDbUrl() {
+        return System.getenv("DATABASE_URL");
     }
 }
